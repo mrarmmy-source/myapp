bf4d7c69e6f8443e05f242ec057278ffbebe6c58
mrarmmyth1379 <mrarmmyth1379@gmail.com>
2025-12-06 13:46:21 +0000

    Uniqlo Shop Design

--- lib/Models/recipe.dart
+++ lib/Models/recipe.dart
@@ -6,10 +6,15 @@ class Recipe {
   Recipe(this.imageUrl, this.imgLabel);
 
   static List<Recipe> samples = [
-    Recipe('assets/images/image1.webp', 'Image 1'),
-    Recipe('assets/images/image2.webp', 'Image 2'),
-    Recipe('assets/images/image3.webp', 'Image 3'),
-    Recipe('assets/images/image5.webp', 'Image 4'),
-    
+    Recipe('assets/images/Uniqlo1.webp', 'Uniqlo 1'),
+    Recipe('assets/images/Uniqlo2.webp', 'Uniqlo 2'),
+    Recipe('assets/images/Uniqlo3.webp', 'Uniqlo 3'),
+    Recipe('assets/images/Uniqlo4.webp', 'Uniqlo 4'),
+    Recipe('assets/images/Uniqlo5.webp', 'Uniqlo 5'),
+    Recipe('assets/images/Uniqlo6.webp', 'Uniqlo 6'),
+    Recipe('assets/images/Uniqlo7.webp', 'Uniqlo 7'),
+    Recipe('assets/images/Uniqlo8.webp', 'Uniqlo 8'),
+    Recipe('assets/images/Uniqlo9.webp', 'Uniqlo 9'),
+    Recipe('assets/images/Uniqlo10.webp', 'Uniqlo 10'),
   ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class RecipeApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(centerTitle: true),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Uniqlo Shop'),
     );
   }
 }
